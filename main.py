9c101986d3251e7b1f7621218ceeace9b440ddec
Fevronija Rybkina <anton.tsizov@tkvg.ee>
2026-03-19 01:05:13 +0200

    graph interface

--- main.py
+++ main.py
@@ -49,10 +49,10 @@ async def send_email_async(subject: str, recipients:str, body:str):
     fast_mail = FastMail(configuracija_pochty)
     await fast_mail.send_message(message)
 @app.post("/api/add_urok_submit", response_model=FastUI,response_model_exclude_none=True)
-async def insert_DB_urok_s_GrIntr(form:Annotated[Urok_Schema,FastUIForm[Urok_Schema]]):
+def insert_DB_urok_s_GrIntr(form:Annotated[Urok_Schema,FastUIForm[Urok_Schema]]):
     print(form)
 @app.post("/api/add_urok", response_model=FastUI,response_model_exclude_none=True)
-async def create_urok_graph_inter() -> list[AnyComponent]:
+def create_urok_graph_inter() -> list[AnyComponent]:
     return [components.Page(components=
                             [components.Heading(text="Добавить урок",level=2),
                              components.ModelForm(model=Urok_Schema,submit_url="/api/add_urok_submit")])]
